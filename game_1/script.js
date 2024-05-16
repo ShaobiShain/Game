9e90062904840b4d28066ce4fb4f5cb1d45ebe1b
ShaobiShain <anatolyivahnenko@yandex.ru>
2024-05-16 22:20:03 +0300

    mb final commit

--- game_1/script.js
+++ game_1/script.js
@@ -2,11 +2,15 @@ window.addEventListener('load', function () {
     // canvas setup
     const canvas = this.document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 1600;
-    canvas.height = 800;    
+    resizeCanvas();
 
     const game = new Game(canvas.width, canvas.height);
     let lastTime = 0;
+
+    const music = new Audio();
+    music.src = "./Assets/music/Tetris.mp3";
+    music.play();
+        
 
     function animate(currentTime) {
         const deltaTime = currentTime - lastTime;
@@ -14,9 +18,18 @@ window.addEventListener('load', function () {
         game.draw(ctx);
         game.update(deltaTime);
         lastTime = currentTime;
-        requestAnimationFrame(animate)
+        requestAnimationFrame(animate);
     }
-    
-    animate(0);
-})
 
+    window.addEventListener('resize', function () {
+        resizeCanvas();
+        game.updateDimensions(canvas.width, canvas.height);
+    });
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
+    animate(0);
+});
--- game_1/src/Game.js
+++ game_1/src/Game.js
@@ -7,32 +7,19 @@ class Game {
         this.ui = new UI(this);
         this.keys = [];
         this.enemies = [];
-        this.ammo = 25;
-        this.maxAmmo = 50;
+        this.ammo = 35;
+        this.maxAmmo = 65;
         this.ammoTimer = 0;
         this.ammoInterval = 300;
         this.enemyTimer = 0;
         this.enemyInterval = 1000;
         this.gameOver = false;
         this.score = 0;
-        this.winningScore = 60;
+        this.winningScore = 91;
         this.gameTime = 0;
         this.timeLimit = 35 * 1000;
         this.background = new Background(this);
         this.speed = 1;
-        this.restartGame = () => {
-            this.player = new Player(this);
-            this.keys = [];
-            this.enemies = [];
-            this.ammo = 20;
-            this.ammoTimer = 0;
-            this.enemyTimer = 0;
-            this.gameOver = false;
-            this.score = 0;
-            this.gameTime = 0;
-            this.background = new Background(this);
-            this.speed = 1;
-        };
     }
 
     update(deltaTime) {
